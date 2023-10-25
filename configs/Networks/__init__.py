a7091c6a722c79c56bab3d3b3b8363cc55fc013b
Hao Ding <hding2455@github.com>
2023-10-24 20:20:18 -0400

    Segformer

--- configs/Networks/__init__.py
+++ configs/Networks/__init__.py
@@ -1,2 +1,3 @@
 from .unet_cts import cfg as UNet_CTS
 from .hrnet_cts import cfg as HRNet_CTS
+from .segformer_cts import cfg as Segformer_CTS
--- configs/__init__.py
+++ configs/__init__.py
@@ -1,10 +1,11 @@
 from .CaRTS import CaRTS_AMBF, CaRTS_CTS
 from .TC_CaRTS import TCCaRTS_CTS
-from .Networks import UNet_CTS, HRNet_CTS
+from .Networks import UNet_CTS, HRNet_CTS, Segformer_CTS
 config_dict = {
         "CaRTS_AMBF": CaRTS_AMBF,
         "CaRTS_CTS": CaRTS_CTS,
         "TCCaRTS_CTS": TCCaRTS_CTS,
         "UNet_CTS": UNet_CTS,
-        "HRNet_CTS": HRNet_CTS
+        "HRNet_CTS": HRNet_CTS,
+        "Segformer_CTS": Segformer_CTS
         }
