dca7325705630b034bb0289bfd6eb25d1941e134
Hao Ding <hding2455@github.com>
2024-02-12 14:27:35 -0500

    remove redundant roation related functions

--- CaRTS/vision/tta.py
+++ CaRTS/vision/tta.py
@@ -1,142 +1,9 @@
 import torch
+import torchvision.transforms.functional as TF
 import itertools
 from functools import partial
 import torch.nn as nn
 from typing import Mapping, Union, List
-
-class functional:
-    def __init__(self):
-            # You can add any attributes or parameters here, or leave it empty if not needed
-            pass
-
-    def rot90(x, k=1):
-        """rotate batch of images by 90 degrees k times"""
-        return torch.rot90(x, k, (2, 3))
-
-
-    def hflip(x):
-        """flip batch of images horizontally"""
-        return x.flip(3)
-
-
-    def vflip(x):
-        """flip batch of images vertically"""
-        return x.flip(2)
-
-
-    def sum(x1, x2):
-        """sum of two tensors"""
-        return x1 + x2
-
-
-    def add(x, value):
-        """add value to tensor"""
-        return x + value
-
-
-    def max(x1, x2):
-        """compare 2 tensors and take max values"""
-        return torch.max(x1, x2)
-
-
-    def min(x1, x2):
-        """compare 2 tensors and take min values"""
-        return torch.min(x1, x2)
-
-
-    def multiply(x, factor):
-        """multiply tensor by factor"""
-        return x * factor
-
-
-    def scale(x, scale_factor, interpolation="nearest", align_corners=None):
-        """scale batch of images by `scale_factor` with given interpolation mode"""
-        h, w = x.shape[2:]
-        new_h = int(h * scale_factor)
-        new_w = int(w * scale_factor)
-        return functional.interpolate(
-            x, size=(new_h, new_w), mode=interpolation, align_corners=align_corners
-        )
-
-
-    def resize(x, size, interpolation="nearest", align_corners=None):
-        """resize batch of images to given spatial size with given interpolation mode"""
-        return functional.interpolate(x, size=size, mode=interpolation, align_corners=align_corners)
-
-
-    def crop(x, x_min=None, x_max=None, y_min=None, y_max=None):
-        """perform crop on batch of images"""
-        return x[:, :, y_min:y_max, x_min:x_max]
-
-
-    def crop_lt(x, crop_h, crop_w):
-        """crop left top corner"""
-        return x[:, :, 0:crop_h, 0:crop_w]
-
-
-    def crop_lb(x, crop_h, crop_w):
-        """crop left bottom corner"""
-        return x[:, :, -crop_h:, 0:crop_w]
-
-
-    def crop_rt(x, crop_h, crop_w):
-        """crop right top corner"""
-        return x[:, :, 0:crop_h, -crop_w:]
-
-
-    def crop_rb(x, crop_h, crop_w):
-        """crop right bottom corner"""
-        return x[:, :, -crop_h:, -crop_w:]
-
-
-    def center_crop(x, crop_h, crop_w):
-        """make center crop"""
-
-        center_h = x.shape[2] // 2
-        center_w = x.shape[3] // 2
-        half_crop_h = crop_h // 2
-        half_crop_w = crop_w // 2
-
-        y_min = center_h - half_crop_h
-        y_max = center_h + half_crop_h + crop_h % 2
-        x_min = center_w - half_crop_w
-        x_max = center_w + half_crop_w + crop_w % 2
-
-        return x[:, :, y_min:y_max, x_min:x_max]
-
-
-    def _disassemble_keypoints(keypoints):
-        x = keypoints[..., 0]
-        y = keypoints[..., 1]
-        return x, y
-
-    def _assemble_keypoints(x, y):
-        return torch.stack([x, y], dim=-1)
-
-    def keypoints_hflip(keypoints):
-        x, y = functional._disassemble_keypoints(keypoints)
-        return functional._assemble_keypoints(1. - x, y)
-
-    def keypoints_vflip(keypoints):
-        x, y = functional._disassemble_keypoints(keypoints)
-        return functional._assemble_keypoints(x, 1. - y)
-
-    def keypoints_rot90(keypoints, k=1):
-
-        if k not in {0, 1, 2, 3}:
-            raise ValueError("Parameter k must be in [0:3]")
-        if k == 0:
-            return keypoints
-        x, y = functional._disassemble_keypoints(keypoints)
-
-        if k == 1:
-            xy = [y, 1. - x]
-        elif k == 2:
-            xy = [1. - x, 1. - y]
-        elif k == 3:
-            xy = [1. - y, x]
-
-        return functional._assemble_keypoints(*xy)
 
 class BaseTransform:
     identity_param = None
@@ -258,9 +125,9 @@ class Merger:
         elif self.type == 'gmean':
             self.output = self.output * x
         elif self.type == 'max':
-            self.output = functional.max(self.output, x)
+            self.output = torch.max(self.output, x)
         elif self.type == 'min':
-            self.output = functional.min(self.output, x)
+            self.output = torch.min(self.output, x)
     
     @property
     def result(self):
@@ -335,12 +202,12 @@ class HorizontalFlip(DualTransform):
         if apply:
             if isinstance(image, dict):
                 image = image['image']
-            image = functional.hflip(image)
+            image = image.flip(3)
         return image
 
     def apply_deaug_mask(self, mask, apply=False, **kwargs):
         if apply:
-            mask = functional.hflip(mask)
+            mask = mask.flip(3)
         return mask
 
     def apply_deaug_label(self, label, apply=False, **kwargs):
@@ -348,7 +215,7 @@ class HorizontalFlip(DualTransform):
 
     def apply_deaug_keypoints(self, keypoints, apply=False, **kwargs):
         if apply:
-            keypoints = functional.keypoints_hflip(keypoints)
+            keypoints = TF.hflip(keypoints)
         return keypoints
     
 class Rotate90(DualTransform):
@@ -370,7 +237,7 @@ class Rotate90(DualTransform):
         k = angle // 90 if angle >= 0 else (angle + 360) // 90
         if isinstance(image, dict):
             image = image['image']
-        return functional.rot90(image, k)
+        return torch.rot90(image, k, (2, 3))
 
     def apply_deaug_mask(self, mask, angle=0, **kwargs):
         return self.apply_aug_image(mask, -angle)
@@ -381,7 +248,7 @@ class Rotate90(DualTransform):
     def apply_deaug_keypoints(self, keypoints, angle=0, **kwargs):
         angle *= -1
         k = angle // 90 if angle >= 0 else (angle + 360) // 90
-        return functional.keypoints_rot90(keypoints, k=k) 
+        return TF.rotate(keypoints, angle=k*90) 
 
 def d4_transform():
     return Compose(
