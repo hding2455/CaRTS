8441b9885ffd3c97f40a4294237a2904fe2eb7db
Hao Ding <hding2455@gmail.com>
2024-04-01 14:02:10 -0400

    Update __init__.py

--- configs/__init__.py
+++ configs/__init__.py
@@ -1,10 +1,7 @@
 from .CTS.CaRTS import CaRTS_AMBF, CaRTS_CTS
 from .CTS.TC_CaRTS import TCCaRTS_CTS
 from .CTS.Networks import UNet_CTS, HRNet_CTS, DeepLabv3_plus_CTS, Segformer_CTS, SETR_Naive_CTS, SETR_MLA_CTS, SETR_PUP_CTS, UNet_ENDOVIS
-from .CTS.Augmentation import UNet_CTS_AutoAugment
-from .CTS.Augmentation import UNet_CTS_Elastic
-from .CTS.Augmentation import UNet_CTS_Projective
-from .CTS.Augmentation import UNet_CTS_Combine
+from .CTS.Augmentation import UNet_CTS_AutoAugment, UNet_CTS_Elastic, UNet_CTS_Projective, UNet_CTS_Combine
 from .SegSTRONGC.Networks import UNet_SegSTRONGC, Segformer_SegSTRONGC
 from .SegSTRONGC.Augmentation import UNet_SegSTRONGC_Projective
 
